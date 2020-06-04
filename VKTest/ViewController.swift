338b0198cae956dcdcd51b4faeca9539a60f72dd
madi <Qzer.1998@gmail.com>
2020-06-04 22:26:28 +0600

    first commit

---
+++ VKTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKTest
+//
+//  Created by Мадияр on 6/4/20.
+//  Copyright © 2020 Мадияр. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
